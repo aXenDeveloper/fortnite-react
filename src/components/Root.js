984fadcffd4e1edf9e83f168afc3d5540375ac8b
aXenDev <58148176+aXenDeveloper@users.noreply.github.com>
2020-11-04 19:35:55 +0100

    Added typescript

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,53 +1,54 @@
 import React, { Component } from 'react';
 import HomeView from '../views/HomeView';
 import ChallengesView from '../views/ChallengesView';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Nav from './Nav';
 import logo from '../assets/logo.png';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import i18n from '../i18n';
 import Lang from './Lang';
 
 class Root extends Component {
-    state = {
-        lang: i18n.language
-    }
+	state = {
+		lang: i18n.language
+	};
 
-    changeLanguage = (lng) => {
-        i18n.changeLanguage(lng);
-        this.setState({
-            lang: i18n.language
-        })
-    }
+	changeLanguage = lng => {
+		i18n.changeLanguage(lng);
+		this.setState({
+			lang: i18n.language
+		});
+	};
 
-    render() {
-        return (
-            <BrowserRouter>
-                <header className="header">
-                    <div className="container">
-                        <div className="header_logo">
-                            <Link to="/">
-                                <img src={logo} alt="Logo" />
-                            </Link>
-                        </div>
+	render() {
+		return (
+			<BrowserRouter>
+				<header className='header'>
+					<div className='container'>
+						<div className='header_logo'>
+							<Link to='/'>
+								<img src={logo} alt='Logo' />
+							</Link>
+						</div>
 
-                        <Nav />
+						<Nav />
 
-                        <Lang changeLanguage={this.changeLanguage} />
-                    </div>
-                </header>
+						<Lang changeLanguage={this.changeLanguage} />
+					</div>
+				</header>
 
-
-                <main>
-                    <Switch>
-                        <Route exact path="/" component={HomeView} />
-                        <Route path="/challenges" component={() => <ChallengesView />} />
-                    </Switch>
-                </main>
-
-            </BrowserRouter>
-        );
-    };
-};
+				<main>
+					<Switch>
+						<Route exact path='/' component={HomeView} />
+						<Route
+							path='/challenges'
+							component={() => <ChallengesView />}
+						/>
+					</Switch>
+				</main>
+			</BrowserRouter>
+		);
+	}
+}
 
 export default Root;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/Root.js';
 import './scss/index.scss';
 import './i18n';
-import Root from './components/Root';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Root />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
